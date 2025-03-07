6c628713990c5447e948ddef1d5c55464b6081e1
Justmenome <mggluke6@gmail.com>
2025-03-07 14:23:18 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,27 @@
 const express = require('express');
-const path = require('path');
 const http = require('http');
 const socketIo = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+
 const io = socketIo(server);
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+app.use(express.static('public'));
 
 io.on('connection', (socket) => {
-  console.log('A user connected');
+  console.log('a user connected');
 
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
   });
 
   socket.on('disconnect', () => {
-    console.log('User disconnected');
+    console.log('user disconnected');
   });
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
